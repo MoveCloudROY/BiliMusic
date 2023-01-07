1ca9d4ba0987f6733011d982a5308a4a701ca7a8
roy1994 <dev.mancitrus@outlook.com>
2023-01-08 00:33:55 +0800

    feat: make some architecture modifies

--- app/src/main/java/top/roy1994/bilimusic/viewmodel/SheetDetailViewModel.kt
+++ app/src/main/java/top/roy1994/bilimusic/viewmodel/SheetDetailViewModel.kt
@@ -2,11 +2,23 @@ package top.roy1994.bilimusic.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import top.roy1994.bilimusic.data.objects.mixed.MusicSheetDao
+import top.roy1994.bilimusic.data.objects.music.MusicEntity
+import top.roy1994.bilimusic.data.objects.sheet.SheetDao
+import top.roy1994.bilimusic.data.objects.sheet.SheetEntity
+import top.roy1994.bilimusic.data.utils.AppDatabase
 
 class SheetDetailViewModel(application: Application) : AndroidViewModel(application) {
+    private val musicSheetDao: MusicSheetDao
 
+    init {
+        val appDb = AppDatabase.getInstance(application)
+        musicSheetDao = appDb.musicSheetDao()
+    }
+    val musicSheetMap: LiveData<Map<SheetEntity, List<MusicEntity>>> = musicSheetDao.loadSheetAndMusics()
 }
 
 class SheetDetailViewModelFactory(
